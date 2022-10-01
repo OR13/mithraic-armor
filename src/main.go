45a735f6329e47f575460626df33ad16e9e386d9
Orie Steele <orie@transmute.industries>
2022-10-01 10:42:18 -0500

    :rocket:

--- src/main.go
+++ src/main.go
@@ -2,9 +2,42 @@
 package main
 
 	
-import "fmt"
+import (
+    "fmt"
+    "github.com/veraison/go-cose"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+)
 
 
 func main() {
-    fmt.Println("hello world")
+    
+    var messageStr = "🌈 I wear the Armor of Mithras and the Light."
+
+    var messageBytes = []byte(messageStr)
+
+    // fmt.Println( messageStr )
+
+    privateKey, _ := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+    signer, _ := cose.NewSigner(cose.AlgorithmES512, privateKey)
+
+    headers := cose.Headers{
+        Protected: cose.ProtectedHeader{
+            cose.HeaderLabelAlgorithm: cose.AlgorithmES512,
+        },
+    }
+
+    sig, _ := cose.Sign1(rand.Reader, signer, headers, messageBytes, nil)
+
+    publicKey := privateKey.Public()
+    verifier, _ := cose.NewVerifier(cose.AlgorithmES512, publicKey)
+
+    var msg cose.Sign1Message
+    _ = msg.UnmarshalCBOR(sig)
+    _ = msg.Verify(nil, verifier)
+
+    fmt.Println( string(msg.Payload) )
+
+    
 }
